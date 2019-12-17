fc3034f827018a09d646a3f7a62c42d67e34b35b
huynhkimtri <huynhkimtri.fuhcm@gmail.com>
2019-12-17 10:06:03 +0700

    add notification for view

---
+++ app/src/main/java/com/duatson/studentapp/Notification1DetailActivity.java
@@ -0,0 +1,24 @@
+package com.duatson.studentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Notification1DetailActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification1_detail);
+        toolbar = findViewById(R.id.tbrNotificationDetail);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
