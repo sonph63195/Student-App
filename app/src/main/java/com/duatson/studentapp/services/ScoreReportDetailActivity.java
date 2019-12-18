6ba0cb537f3f062da70cb1c5f462094a7278aa9e
sonph63195 <sonpham3798@gmail.com>
2019-12-18 15:29:07 +0700

    score report

--- app/src/main/java/com/duatson/studentapp/services/ScoreReportDetailActivity.java
+++ app/src/main/java/com/duatson/studentapp/services/ScoreReportDetailActivity.java
@@ -3,8 +3,12 @@ package com.duatson.studentapp.services;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.duatson.studentapp.R;
+import com.google.android.material.chip.Chip;
 
 public class ScoreReportDetailActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class ScoreReportDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_report_detail);
+    }
+
+    public void clickToCancel(View view) {
+        Toast.makeText(this, "Hủy thành công", Toast.LENGTH_SHORT).show();
+        Chip chipConfirmStatus = findViewById(R.id.chipConfirmStatus);
+        chipConfirmStatus.setText("Đã hủy");
+        Button btnCancel = findViewById(R.id.btnCancel);
+        btnCancel.setVisibility(View.INVISIBLE);
     }
 }
