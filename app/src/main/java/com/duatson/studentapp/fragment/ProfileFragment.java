99ab4d4d48dbcbede2b107fad5e0093892a10388
sonph63195 <sonpham3798@gmail.com>
2019-12-19 09:45:42 +0700

    sonph - add more field

--- app/src/main/java/com/duatson/studentapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/duatson/studentapp/fragment/ProfileFragment.java
@@ -3,11 +3,14 @@ package com.duatson.studentapp.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.duatson.studentapp.R;
 
@@ -16,6 +19,7 @@ import com.duatson.studentapp.R;
  */
 public class ProfileFragment extends Fragment {
 
+    RelativeLayout btnSignOut;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,7 +30,9 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
     }
 
 }
